7dddfd31bca13b1ee98ce9387f631e4407b3c4ac
wharlen <wharlen.santana@direcional.com.br>
2023-05-19 12:02:58 -0300

    Fix: correcoes de validacao de numero de doc e design lwc

--- force-app/main/default/lwc/edicaoCamposCliente/edicaoCamposCliente.js
+++ force-app/main/default/lwc/edicaoCamposCliente/edicaoCamposCliente.js
@@ -1,8 +1,8 @@
 import { LightningElement, api, wire, track} from 'lwc';
-import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import { getRecord, getFieldValue, updateRecord } from 'lightning/uiRecordApi';
 //getPicklistValues para retornar os valores das picklists
 //getObjectInfo para retornar o parametro obrigatório do getPicklistValues que é o recordTypeId
-import { getPicklistValues, getObjectInfo } from 'lightning/uiObjectInfoApi'
+import {  getObjectInfo } from 'lightning/uiObjectInfoApi'
 
 import ACCOUNT_OBJECT from '@salesforce/schema/Account';
 import IDCONTA from '@salesforce/schema/Account.Id';
@@ -17,6 +17,7 @@ const fields = [IDCONTA, TIPO, TIPOCONTA, NOME, NUMEROCONTA, RECORDTYPEID];
 export default class EdicaoCamposCliente extends LightningElement {
     @api recordId;
     @api objectInfo;
+    @api isLoading = false;
     @track account;
     @api tipoContaValues =[
         {'label':'CNPJ', 'value':'CNPJ'},
@@ -55,6 +56,10 @@ export default class EdicaoCamposCliente extends LightningElement {
     @wire(getPicklistValues, { recordTypeId: recordType, fieldApiName: TIPOCONTA })
     tipoContaValues;*/
 
+    get id() {
+        return getFieldValue(this.account.data, IDCONTA);
+    }
+
     get tipo() {
         return getFieldValue(this.account.data, TIPO);
     }
@@ -75,23 +80,28 @@ export default class EdicaoCamposCliente extends LightningElement {
         return getFieldValue(this.account.data.recordTypeId, RECORDTYPE);
     }
 
-    updateAccount() {
-        const allValid = [...this.template.querySelectorAll('lightning-input')]
+    updateAccount(event) {
+        this.isLoading = true;
+        console.log('tetsetsets')
+        console.log(this.account.data.Id)
+        console.log(this.recordId)
+        /*const allValid = [...this.template.querySelectorAll('lightning-input')]
             .reduce((validSoFar, inputFields) => {
                 inputFields.reportValidity();
                 return validSoFar && inputFields.checkValidity();
-            }, true);
+            }, true);*/
 
-        if (allValid) {
+       //if (allValid) {
             const fields = {};
-            fields[IDCONTA.fieldApiName] = this.account.Id;
+            fields[IDCONTA.fieldApiName] = this.recordId;
             fields[TIPO.fieldApiName] = this.template.querySelector("[data-field='tipo']").value;
             fields[TIPOCONTA.fieldApiName] = this.template.querySelector("[data-field='tipoConta']").value;
             fields[NOME.fieldApiName] = this.template.querySelector("[data-field='nome']").value;
             fields[NUMEROCONTA.fieldApiName] = this.template.querySelector("[data-field='numeroConta']").value;
+            console.log(fields)
 
             const recordInput = { fields };
-
+            console.log(recordInput)
             updateRecord(recordInput)
                 .then(() => {
                     this.dispatchEvent(
@@ -111,8 +121,10 @@ export default class EdicaoCamposCliente extends LightningElement {
                             variant: 'error'
                         })
                     );
+                }).finally(()=>{
+                    this.isLoading = false;
                 });
-            }
+           /*}
         else {
             this.dispatchEvent(
                 new ShowToastEvent({
@@ -121,7 +133,7 @@ export default class EdicaoCamposCliente extends LightningElement {
                     variant: 'error'
                 })
              );
-        }
+        }*/
     }
 
 }
--- force-app/main/default/triggers/AccountTrigger.trigger
+++ force-app/main/default/triggers/AccountTrigger.trigger
@@ -1,11 +1,14 @@
-trigger AccountTrigger on Account (before insert) {
+trigger AccountTrigger on Account (before insert, before update, after insert) {
     if(Trigger.isBefore && Trigger.isInsert){
         AccountBO.validaTipoConta(Trigger.new);
-        AccountBO.trataTipoParceiro(Trigger.new);
-        AccountBO.trataTipoConsumidorFinal(Trigger.new);
     }
 
     if(Trigger.isBefore && Trigger.isUpdate){
         AccountBO.validaTipoConta(Trigger.new);
+    }
+
+    if(Trigger.isAfter && Trigger.isInsert){
+        AccountBO.trataTipoParceiro(Trigger.new);
+        AccountBO.trataTipoConsumidorFinal(Trigger.new);
     }
 }
